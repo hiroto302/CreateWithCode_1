7761d05ae90e3f796acf3ddb2eee3c8029b8e5b7
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-20 18:31:49 +0900

    Add Function FollowPlayer.cs

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Vector3 _offset;
+
+    public Transform target;
+
+    void Start()
+    {
+        _offset = transform.position;
+    }
+
+    void Update()
+    {
+        // Offset the camera behind the player(car) by adding to player's position
+        transform.position = target.position + _offset;
+    }
+}
