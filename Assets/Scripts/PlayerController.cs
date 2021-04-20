396df12f538155daf70993baded211d49c313961
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-20 18:19:32 +0900

    Create Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0, 30), SerializeField]
+    int _forwardSpeed  = 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * _forwardSpeed);
+    }
+}
