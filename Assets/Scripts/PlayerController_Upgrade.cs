656c3b173b45da577f204546c8103513601bb308
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-23 23:35:36 +0900

    車の物理的移動方法, Center of mass, AddRelativeForce

---
+++ Assets/Scripts/PlayerController_Upgrade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Upgrade : MonoBehaviour
+{
+    [SerializeField]
+    float _horsePower  = 20000, _turnSpeed = 25;
+
+    [SerializeField]
+    float _horizontalInput, _verticalInput;
+    Rigidbody _playerRb;
+    [SerializeField] GameObject _centerOfMass;
+
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _playerRb.centerOfMass = _centerOfMass.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        // Moves the car forward based on vertical input
+        // AddRelativeForce : ローカル座標に対して Rigidbody に相対的な力を加える
+        _playerRb.AddRelativeForce(Vector3.forward * _horsePower * _verticalInput);
+
+        // Moves the car horizontal based on horizontal input
+        transform.Rotate(Vector3.up , Time.deltaTime * _turnSpeed * _horizontalInput);
+    }
+}
