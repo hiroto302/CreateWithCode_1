9098697a248e0a424b4b807aa0abc667df103340
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-24 01:17:46 +0900

    全ての車輪が地面にある時、車に新たな馬力を加えることができる
    空中にあるときも、車は加速、減速、ターンをしたり、スピードや RPM が増加しないようにする

--- Assets/Scripts/PlayerController_Upgrade.cs
+++ Assets/Scripts/PlayerController_Upgrade.cs
@@ -16,6 +16,9 @@ public class PlayerController_Upgrade : MonoBehaviour
     [SerializeField] TextMeshProUGUI _rpmText;
     [SerializeField] float _rpm;  // MPH : 速度 Miles per hour（マイル毎時）の略。mi/hとも表記される
 
+    [SerializeField] List<WheelCollider> _allWheels;
+    [SerializeField] int _wheelsOnGround;
+
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
@@ -27,13 +30,15 @@ public class PlayerController_Upgrade : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
+        if(IsOnGround())
+        {
+            // Moves the car forward based on vertical input
+            // AddRelativeForce : ローカル座標に対して Rigidbody に相対的な力を加える
+            _playerRb.AddRelativeForce(Vector3.forward * _horsePower * _verticalInput);
 
-        // Moves the car forward based on vertical input
-        // AddRelativeForce : ローカル座標に対して Rigidbody に相対的な力を加える
-        _playerRb.AddRelativeForce(Vector3.forward * _horsePower * _verticalInput);
-
-        // Moves the car horizontal based on horizontal input
-        transform.Rotate(Vector3.up , Time.deltaTime * _turnSpeed * _horizontalInput);
+            // Moves the car horizontal based on horizontal input
+            transform.Rotate(Vector3.up , Time.deltaTime * _turnSpeed * _horizontalInput);
+        }
 
         // get the correct speed by using rigidbody.velocity.magnitude. It returns the speed in M/S.
         // To get the speed in KM/H multiply the magnitude by 3.6f, and to get it in M/H multiply the magnitude by 2.237f.
@@ -42,5 +47,25 @@ public class PlayerController_Upgrade : MonoBehaviour
 
         _rpm = Mathf.Round((_speed % 30) * 40);
         _rpmText.SetText("RPM: " + _rpm.ToString());
+    }
+
+    bool IsOnGround()
+    {
+        _wheelsOnGround = 0;
+        foreach(var wheel in _allWheels)
+        {
+            if(wheel.isGrounded)
+            {
+                _wheelsOnGround++;
+            }
+        }
+        if(_wheelsOnGround == 4)
+        {
+            return  true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
