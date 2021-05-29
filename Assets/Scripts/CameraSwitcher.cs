cd77ec43b700ecd56bc42006d75adfc0a87fc061
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-29 14:19:15 +0900

    Create Camera switcher

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] GameObject _subCamera;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCamera();
+        }
+    }
+
+    // カメラの切り替え
+    void SwitchCamera()
+    {
+        bool isActive = _subCamera.activeSelf;
+        _subCamera.SetActive(!isActive);
+    }
+}
