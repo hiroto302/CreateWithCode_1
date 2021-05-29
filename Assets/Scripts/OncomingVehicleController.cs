f3a46484aecfdcbc06f11253e6e653fa436c5d77
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-29 13:53:23 +0900

    Create Oncoming Vehicle Prefab & Controller

---
+++ Assets/Scripts/OncomingVehicleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 対向車を制御するクラス
+// 直線に一定の速度で走らせる
+public class OncomingVehicleController : MonoBehaviour
+{
+    [SerializeField] float _forwardSpeed = 20.0f;
+
+    Rigidbody _myRb;
+    [SerializeField] GameObject _centerOfMass;
+
+    void Start()
+    {
+        _myRb = GetComponent<Rigidbody>();
+        // _myRb.centerOfMass = _centerOfMass.transform.position;
+    }
+
+    void Update()
+    {
+        // この車のローカルのz軸方向を取得して、それにスピードをかける
+        _myRb.velocity = transform.forward * _forwardSpeed;
+    }
+
+}
